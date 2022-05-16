a8468784b9d27f78fd4e6fd25af692958a59490e
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-05-16 03:28:24 -0700

    add menu

--- cipher.py
+++ cipher.py
@@ -3,3 +3,16 @@ def getName():
     name = str(input(" "))
     print("Hello, name")
     return name
+
+    # to display a menu
+ 
+print('''
+1. Instantiate encryption parameters
+2. Encrypt
+3. Decrypt text
+4. Reset encryption parameters
+5. History
+''')
+
+print("Enter the number that corresponds with the task you want to perform ")
+choice = int(input( ))
