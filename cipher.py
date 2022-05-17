b336c433aca29749731e4ee1091152b3a17e287e
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-05-17 06:15:27 -0700

    add encrypt feature and some improvements

--- cipher.py
+++ cipher.py
@@ -22,7 +22,31 @@ def instantiate():
 
 
 def encrypt():
-    pass
+    plainText = input('ENTER MESSAGE: ')
+    # The key here means that the moving steps of the text so if the user enters letter A it will move to letter E
+    key = jumps
+    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # this is where the message appears after it has been encrypted
+    cypherText = ' '
+
+    plainText = plainText.upper()
+
+    for letter in plainText:
+        # we iterate throught the inputted values first
+        if letter in ALPHABET:
+            # then we check if the letters are contained in the alphabets using the find function
+            num = ALPHABET.find(letter)
+            # when if finds the letter it will add the key to it which are the steps so if it finds letter A  the next letter becomes E
+            num = num + key
+            if num >= len(ALPHABET):
+                # meaning if the keys are moved become longer than the alphabet
+                num = num - len(ALPHABET)
+            elif num < 0:
+                num = num + len(ALPHABET)
+            cypherText = cypherText + ALPHABET[num]
+        else:
+            cypherText = cypherText + letter
+    print(cypherText)
 
 
 def decrypt():
@@ -59,6 +83,7 @@ while (choice.lower() != "0"):
 
     elif choice == "2":
         encrypt()
+        input("Press Enter to continue...")
 
     elif choice == "0":
         print("Exit")
