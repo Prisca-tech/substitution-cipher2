ede3020b3aa3164e432639664c9185e881138749
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-05-17 09:14:09 -0700

    account for upper and lower case

--- cipher.py
+++ cipher.py
@@ -22,14 +22,16 @@ def instantiate():
 
 
 def encrypt():
+    raw_input = input
     plainText = input('ENTER MESSAGE: ')
     # The key here means that the moving steps of the text so if the user enters letter A it will move to letter E
     key = jumps
-    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     # this is where the message appears after it has been encrypted
     cypherText = ' '
 
-    plainText = plainText.upper()
+    plainText = plainText
 
     for letter in plainText:
         # we iterate throught the inputted values first
@@ -50,31 +52,7 @@ def encrypt():
 
 
 def decrypt():
-    plainText = input('ENTER MESSAGE: ')
-    # The key here means that the moving steps of the text so if the user enters letter A it will move to letter E
-    key = jumps
-    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    # this is where the message appears after it has been encrypted
-    cypherText = " "
-
-    plainText = plainText.upper()
-
-    for letter in plainText:
-        # we iterate throught the inputted values first
-        if letter in ALPHABET:
-            # then we check if the letters are contained in the alphabets using the find function
-            num = ALPHABET.find(letter)
-            # when if finds the letter it will add the key to it which are the steps so if it finds letter A  the next letter becomes E
-            num = num - key
-            if num >= len(ALPHABET):
-                # meaning if the keys are moved become longer than the alphabet
-                num = num - len(ALPHABET)
-            elif num < 0:
-                num = num + len(ALPHABET)
-            cypherText = cypherText + ALPHABET[num]
-        else:
-            cypherText = cypherText + letter
-    print(cypherText)
+    pass
 
 
 def reset():
@@ -107,10 +85,6 @@ while (choice.lower() != "0"):
 
     elif choice == "2":
         encrypt()
-        input("Press Enter to continue...")
-
-    elif choice == "3":
-        decrypt()
         input("Press Enter to continue...")
 
     elif choice == "0":
